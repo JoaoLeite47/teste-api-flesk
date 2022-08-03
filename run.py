f7cf7e31269996c887fff1a4c4f87ccdeca1c15a
JoaoLeite47 <StalkerJJ@outlook.com>
2022-08-03 14:39:55 -0300

    all<feat> new method and new route

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)  # create a new instance of the Flask class
 
 
@@ -7,9 +8,24 @@ def hello():  # function decorator
     return "Hello World!"
 
 
-@app.route('/<int:id>') # route decorator
-def hello_id(id): # function decorator
-    return jsonify({'O id é': id}) # jsonify converts a Python object into a JSON string
+@app.route('/<int:id>')  # route decorator
+def hello_id(id):  # function decorator
+    # jsonify converts a Python object into a JSON string
+    return jsonify({'O id é': id})
+
+
+@app.route('/<string:name>')  # route decorator
+def hello_name(name):  # function decorator
+    # jsonify converts a Python object into a JSON string
+    return jsonify({'O nome é': name})
+
+
+@app.route('/soma', methods=['POST'])  # route decorator with  post method
+def soma():  # function decorator
+    # receive data from request on json format
+    dados = json.loads(request.data)
+    # return jsonify with the sum of the two numbers
+    return jsonify({'Soma': dados})
 
 
 if __name__ == '__main__':  # if this file is run directly
